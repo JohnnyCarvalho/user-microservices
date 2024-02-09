6d6ee9ffdb1aeebc0ec52bf070c6ebe054b7115a
JohnnyCarvalho <johnnycarvalhodev@gmail.com>
2024-02-09 19:57:12 -0300

    Initial unitary tests.

--- backend/register-user-api/src/test/java/com/ecommerce/registeruserapi/services/ServicesTest.java
+++ backend/register-user-api/src/test/java/com/ecommerce/registeruserapi/services/ServicesTest.java
@@ -1,33 +1,119 @@
 package com.ecommerce.registeruserapi.services;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import com.ecommerce.registeruserapi.dto.request.UserCreateRequest;
+import com.ecommerce.registeruserapi.dto.response.UserCreateResponse;
+import com.ecommerce.registeruserapi.entities.User;
+import com.ecommerce.registeruserapi.exception.AlreadyExistException;
+import com.ecommerce.registeruserapi.exception.KeyMessageException;
+import com.ecommerce.registeruserapi.exception.UserBlockedException;
 import com.ecommerce.registeruserapi.producer.UserProducer;
 import com.ecommerce.registeruserapi.repositories.UserRepository;
-import jakarta.transaction.Transactional;
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import java.util.Optional;
+import org.junit.jupiter.api.Test;
+import org.mockito.Spy;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.boot.test.mock.mockito.MockBean;
 
-@ActiveProfiles("test")
-@RunWith(SpringRunner.class)
 @SpringBootTest
-@Transactional
 public class ServicesTest {
 
-    @Autowired
+    @MockBean
+    private UserProducer producer;
+
+    @Spy
     private UserRepository repository;
 
     @Autowired
-    private UserProducer producer;
+    private UserService service;
 
     @Autowired
-    private PasswordEncoder passwordEncoder;
+    private ModelMapper modelMapper;
 
     @Test
-    void ShouldCreateUserInDataBase() {
+    public void shouldCreateUserInDataBase() {
+        UserCreateRequest request = ServicesMock.createUserModelRequest();
+        UserCreateResponse responseExpect = ServicesMock.createUserModelResponse();
 
+        doNothing().when(producer).publishMessageEmail(request);
+        when(repository.findByUserName(eq(request.getUserName()))).thenReturn(Optional.empty());
+        when(repository.findByEmail(eq(request.getEmail()))).thenReturn(Optional.empty());
+
+        UserCreateResponse response = service.createUser(request);
+        Optional<User> userAlready = repository.findByUserName(request.getUserName());
+        Optional<User> emailAlready = repository.findByEmail(request.getEmail());
+
+        assertEquals(responseExpect.getUserName(), response.getUserName());
+        assertEquals(responseExpect.getStatus(), response.getStatus());
+        assertFalse(userAlready.isPresent());
+        assertFalse(emailAlready.isPresent());
+        assertNotNull(response);
+        assertNotNull(userAlready);
+        assertNotNull(emailAlready);
+        verify(repository, times(2)).findByUserName(eq(request.getUserName()));
+        verify(repository, times(2)).findByEmail(eq(request.getEmail()));
+        verify(producer).publishMessageEmail(request);
+    }
+
+    @Test
+    public void shouldReturnOneAlreadyExistExceptionWithMessageUserNameAlreadyExist() {
+        UserCreateRequest request = ServicesMock.createUserModelRequest();
+
+        when(repository.findByUserName(eq(request.getUserName()))).thenReturn(Optional.of(new User()));
+
+        assertThatThrownBy(() -> service.createUser(request))
+                .isInstanceOf(AlreadyExistException.class)
+                .hasMessage(KeyMessageException.USER_NAME_ALREADY_EXIST.getKey());
+
+        verify(repository).findByUserName(eq(request.getUserName()));
+    }
+
+    @Test
+    public void shouldReturnOneAlreadyExistExceptionWithMessageEmailAlreadyExist() {
+        UserCreateRequest request = ServicesMock.createUserModelRequest();
+
+        when(repository.findByEmail(eq(request.getEmail()))).thenReturn(Optional.of(new User()));
+
+        assertThatThrownBy(() -> service.createUser(request))
+                .isInstanceOf(AlreadyExistException.class)
+                .hasMessage(KeyMessageException.EMAIL_ALREADY_EXIST.getKey());
+
+        verify(repository).findByEmail(eq(request.getEmail()));
+
+    }
+
+    @Test
+    public void shouldReturnOneUserBlockedExceptionWithMessageUserBlocked() {
+
+        //TODO PAREI AQUI, TENTANDO TESTAR O MÉTODO USUÁRIO BLOQUEADO
+
+        //Arrange
+        UserCreateRequest requestBefore = ServicesMock.createUserBlockedModelRequest();
+        User request = modelMapper.map(requestBefore, User.class);
+        request.setStatus(false);
+
+        when(repository.findByUserName(request.getUserName())).thenReturn(Optional.empty());
+
+        //Act
+        repository.save(request);
+
+        assertThatThrownBy(() -> repository.save(request))
+                .isInstanceOf(UserBlockedException.class)
+                        .hasMessage(KeyMessageException.USER_BLOCKED.getKey());
+
+        //Assert
+        verify(repository).findByUserName(request.getEmail());
     }
 }
